099a4acb71519a4930b52c8651bc5db53000c38d
Craig Gilchrist <craig.a.gilchrist@gmail.com>
2022-06-12 14:25:17 +0100

    Filter old sessions into booked/available so we can prioritise ‘booked’

--- session_mapper.rb
+++ session_mapper.rb
@@ -12,7 +12,7 @@ class SessionMapper
     @new_times = new_times
   end
 
-  def migrate_sessions
+  def migrate_sessions_naive
     old_sessions.each_with_object({}).with_index do |(old_slot, new_mappings), index|
       new_mappings[old_slot.starts_at] = new_sessions[index].tap do |new_slot|
         new_slot.set_state(old_slot.state)
@@ -20,12 +20,29 @@ class SessionMapper
     end
   end
 
-  def old_sessions
-    @old_sessions ||= Slot::Old.collection(old_times).reject(&:suspended?)
+  def migrate_sessions
+    puts "New Sessions: #{new_sessions.count}"
+    puts "Old Sessions: #{old_sessions.count}"
+    puts "      Booked: #{old_sessions_booked.count}"
+    puts "   Available: #{old_sessions_available.count}"
   end
+
+  private
 
   def new_sessions
     @new_sessions ||= Slot::New.collection(new_times)
+  end
+
+  def old_sessions_booked
+    @old_sessions_booked = old_sessions.select(&:booked?)
+  end
+
+  def old_sessions_available
+    @old_sessions_available = old_sessions.select(&:available?)
+  end
+
+  def old_sessions
+    @old_sessions ||= Slot::Old.collection(old_times)
   end
 end
 
@@ -43,17 +60,21 @@ module Slot
       @state = opts[:state] || "available"
     end
 
-    def suspended?
-      state == "suspended"
-    end
-
     def set_state(new_state)
       @state = new_state
     end
   end
 
   class New < Base; end
-  class Old < Base; end
+  class Old < Base
+    def booked?
+      state == "booked"
+    end
+
+    def available?
+      state == "available"
+    end
+  end
 end
 
 old_times = [{
